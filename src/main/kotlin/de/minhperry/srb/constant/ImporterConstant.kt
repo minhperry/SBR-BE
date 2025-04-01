3e233c488ecdcd2f0cbcb7c488a58b4d57340546
minhperry <minhpt2002@gmail.com>
2025-04-01 16:58:55 +0200

    add basic const and service

---
+++ src/main/kotlin/de/minhperry/srb/constant/ImporterConstant.kt
@@ -0,0 +1,6 @@
+package de.minhperry.srb.constant
+
+object ImporterConstant {
+    const val REMOTE_REPO = "https://github.com/NotEnoughUpdates/NotEnoughUpdates-REPO/"
+    const val LOCAL_REPO = "/data/NEU-REPO"
+}
